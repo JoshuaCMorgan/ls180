aea2b296439a8d0b949887b7f1df3a71a342e4ba
Joshua Morgan <jmorg0605@gmail.com>
2021-07-13 14:10:27 -0400

    completed solutions

---
+++ sql_exercises/03_ddl/03.sql
@@ -0,0 +1,7 @@
+-- Increase Star Name Length
+
+-- Hmm... it turns out that 25 characters isn't enough room to store a star's complete name. For instance, the star "Epsilon Trianguli Australis A" requires 30 characters. Modify the column so that it allows star names as long as 50 characters.
+
+ALTER TABLE stars
+  ALTER COLUMN name 
+  TYPE varchar(50); 
